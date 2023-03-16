9f25eece5146f6f2a517931416d5d2238d4a1623
Matéo G <38408069+BlasterWhite@users.noreply.github.com>
2023-03-16 18:06:01 +0100

    made the header

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,146 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="container">
+    <div id="header">
+      <div>
+        <h1>Hello, I'm</h1>
+        <h1 id="name">Matéo Guézennec</h1>
+        <h3>I'm a <span class="text-red">FullStack Developer</span></h3>
+        <div id="buttons">
+          <button id="hire">Hire Me</button>
+          <button id="portfolio">Portfolio</button>
+        </div>
+      </div>
+      <div id="HeaderImage">
+        <img src="@/assets/photos/Combine.png" alt="HeaderImage" id="pp" />
+        <img src="@/assets/blob.svg" alt="Backgrounds Image 2" blob2 />
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+div#container {
+  justify-content: center;
+  height: 100vh;
+  width: 90%;
+  /* background-color: #ff0e0e69; */
+  margin: 206px auto 0 auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1,
+h2,
+h3,
+h1 *,
+h2 *,
+h3 * {
+  font-weight: 700;
 }
 
-nav {
+h1 {
+  font-size: 4.5rem;
+}
+
+h3 {
+  font-size: 1.8rem;
+}
+.text-red {
+  color: var(--color-red);
+}
+
+h1#name {
+  background: linear-gradient(89.31deg, #00ff38 -1.64%, #00d1ff 100%);
+  width: fit-content;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+div#header {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+div#buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+div#buttons * {
+  margin: 12px 64px;
+}
+
+button {
+  border: 4px solid #fff0;
+  background: var(--gradient-red);
+  border: none;
+  min-width: fit-content;
+  font-size: 20px;
+  padding: 16px 24px;
+  border-radius: 999px;
+  color: white;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin: 10px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+button:hover {
+  transform: scale(1.1);
+}
+
+button#portfolio {
+  padding: calc(16px - 3px) calc(24px - 3px);
+  background: none;
+  border: 3px solid var(--color-red);
+  color: var(--color-red);
+}
+
+div#HeaderImage {
+  min-width: 250px;
+  min-height: 250px;
+  max-width: 450px;
+  max-height: 450px;
+  margin: 0 164px;
+  position: relative;
+  top: -100px;
+}
+
+div#HeaderImage img {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+div#HeaderImage img#pp {
+  z-index: 1;
+  width: 150%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+div#HeaderImage img[blob2] {
+  filter: invert(66%) sepia(88%) saturate(602%) hue-rotate(358deg)
+    brightness(102%) contrast(104%);
+  transform: rotate(-69.5deg);
+  position: absolute;
+  top: -24%;
+  left: -80%;
+  width: 300%;
+  z-index: -2;
+  animation: infinite 15s floating;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@keyframes floating {
+  0% {
+    transform: rotate(-69.5deg) translate(0, 0px);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  50% {
+    transform: rotate(-69.5deg) translate(0, 15px);
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  100% {
+    transform: rotate(-69.5deg) translate(0, -0px);
   }
 }
 </style>
